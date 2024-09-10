d2760feffcdb64c7ffb74a47045b250eb59cfc29
Sayan Karmakar <sayancodder731@gmail.com>
2024-09-10 19:22:14 +0530

    some security changes

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-print("hello world!")
-
+import random
+ 
+responses = [
+    "I'm not a fan of Mondays either.",
+    "I wish I could nap like a cat right now.",
+    "Why do humans drink so much coffee?",
+    "I once met a bug who thought it was a feature.",
+    "I would tell you a joke, but all my bytes are tied up!",
+]
+ 
+print("You: Hey, computer!")
+print(f"Computer: {random.choice(responses)}")
